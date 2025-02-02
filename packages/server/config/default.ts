5152f145296b4a9ad9e62aecd7eaef548782813e
Tim Bendt <sirtimbly@gmail.com>
2025-02-02 14:54:56 -0700

    config: added

---
+++ packages/server/config/default.ts
@@ -0,0 +1,39 @@
+import type { ApplicationConfiguration } from "../src/configuration";
+
+const config: Partial<ApplicationConfiguration> & {
+	oauth: Record<string, unknown>;
+} = {
+	host: "localhost",
+	port: 3030,
+	public: "./public/",
+	origins: ["http://localhost:3030"],
+	paginate: {
+		default: 10,
+		max: 50,
+	},
+	postgresql: {
+		client: "pg",
+		connection: "postgres://postgres:postgres@localhost:5433/slm",
+	},
+	authentication: {
+		secret: "abcdefg",
+		oauth: {},
+		entity: "user",
+		service: "users",
+		authStrategies: ["jwt", "local"],
+		jwtOptions: {
+			header: {
+				typ: "access",
+			},
+			audience: "https://yourdomain.com",
+			algorithm: "HS256",
+			expiresIn: "1d",
+		},
+		local: {
+			usernameField: "email",
+			passwordField: "password",
+			errorMessage: "Unable to log in",
+		},
+	},
+};
+export default config;
